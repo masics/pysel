40343f1b71459e0b2235a8ba19dd83e5e51d3f9c
Sam <github@nicholaslau.com>
2021-05-01 21:44:32 +1000

    add 18 20

--- quickinstall.sh
+++ quickinstall.sh
@@ -11,7 +11,7 @@ echo -e 'DONE\nCreating Team ID Changer...'
 chmod 777 /usr/local/bin/
 echo 'Skills Contestant' > /usr/local/bin/pysel/TEAM
 
-echo 'Ubuntu 16.04 - Using systemd'
+echo 'Ubuntu 16.04, 18.04, 20.04 - Using systemd'
 cp static/pysel_scoring.service /etc/systemd/system/
 systemctl enable pysel_scoring.service
 systemctl start pysel_scoring.service
