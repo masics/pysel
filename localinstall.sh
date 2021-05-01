34a48dd2833077ff94f1165c945ff6ae7ea6c78c
Sam <github@nicholaslau.com>
2021-05-02 09:26:39 +1000

    static files

--- localinstall.sh
+++ localinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #user ubuntu
 mkdir /home/ubuntu/Desktop
-cp /static/ForensicQuestion?.txt /home/ubuntu/Desktop
-cp /static/Readme.txt /home/ubuntu/Desktop
+cp static/ForensicQuestion?.txt /home/ubuntu/Desktop
+cp static/Readme.txt /home/ubuntu/Desktop
